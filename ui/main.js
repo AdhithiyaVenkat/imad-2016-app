a1378fabd2d22ad779538a4cdd32d145f04c5094
AdhithiyaVenkat <adhithiyakannan@gmail.com>
2016-10-04 19:39:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var element = document.getElementById('main-text');
+element.innerHtml='New Value';
+
+//Moving an Image
+var img = document.getElementById('madi');
+img.onclick = function () {
+    img.style.marginLeft= '100px';
+}
