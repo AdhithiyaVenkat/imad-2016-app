505b5d61e4a26f6ea1dfa2a08cc24f3ca70fdddb
AdhithiyaVenkat <adhithiyakannan@gmail.com>
2016-10-11 20:01:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,21 @@ button.onclick = function() {
     request.send(null);
 };    
 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+  //Make a request to server and send the name
+  
+  //Display a list of names
+  var names = ['name1', 'name2', 'name3'];
+  var list = '';
+  for (var i=0; i<names.length; i++)
+  {
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul= list;
+};
+    
+};
