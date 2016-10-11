06ca63fb5d7f60a5ab942a88da6fc083fd22e7a9
AdhithiyaVenkat <adhithiyakannan@gmail.com>
2016-10-11 19:18:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,27 @@
+//Counter Code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function() {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+  
+  //Create a request object to counter end point
+  var request= new XMLHttpRequest();
+  
+  //Capture the request and store it in a variable
+  request.onreadystatechange = function() {
+      if( request.readystate === XMLHttpRequest.DONE );
+      //Take some action
+      {
+          var counter;
+          if(request.status === 200)
+            counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+      }
+      //Not yet done
+     }; 
 };
+
+//Make a request
+request.open('GET' , 'http://adhithiyavenkat.imad.hasura-app.io/counter' , true);
+request.send(null);
+    
+
